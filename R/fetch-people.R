b2ef29178317994c5348ad52b26b5d190167ce77
Emi Tanaka <dr.emi.tanaka@gmail.com>
2020-04-05 14:01:14 +1000

    fetch people data script

--- R/fetch-people.R
+++ R/fetch-people.R
@@ -4,7 +4,6 @@
 
 # the fetching is automated with github actions [PLAN]
 
-
 library(googlesheets4)
 library(tidyverse)
 library(glue)
@@ -13,9 +12,10 @@ library(rlang)
 
 sheets_deauth()
 
+fence <- "---"
+
 # the minimum requirement of a person is 
 # First Name, Last Name, Email, Affiliation, and Position
-fence <- "---"
 minimum_template <- "
                      authors:
                      - {user}
@@ -30,50 +30,79 @@ social_template <- function(x, type) {
   social <- switch(type,
                    website = glue("- icon: link",
                                   "  icon_pack: fas",
-                                  "  link: {x}", .trim = FALSE, .sep = "\n"),
+                                  "  link: {x}", 
+                                  .trim = FALSE, .sep = "\n"),
                    email = glue("- icon: envelope",
                                 "  icon_pack: fas",
-                                "  link: mailto:{x}", .trim = FALSE, .sep = "\n"),
-                   github = glue("- icon: envelope",
+                                "  link: \"mailto:{x}\"", 
+                                .trim = FALSE, .sep = "\n"),
+                   scholar = glue("- icon: google-scholar",
+                                "  icon_pack: ai",
+                                "  link: {x}", 
+                                .trim = FALSE, .sep = "\n"),
+                   twitter = glue("- icon: twitter",
+                                  "  icon_pack: fab",
+                                  "  link: https://twitter.com/{x}", 
+                                  .trim = FALSE, .sep = "\n"),
+                   github = glue("- icon: github",
                                  "  icon_pack: fab",
-                                 "  link: https://github.com/{x}", .trim = FALSE, .sep = "\n"))
+                                 "  link: https://github.com/{x}", 
+                                 .trim = FALSE, .sep = "\n"))
   social[is.na(x)] <- ""
   social
 } 
 # the list of groups that the user belongs to
 # List: 
 # Current, Alumni
-user_groups <- function(...) {
-  flist <- list2(...)
-  glue("user_groups:\n", paste("-", flist, collapse = "\n"))
+user_groups <- function(types) {
+  group <- map_chr(types, 
+                   ~switch(.x, 
+                      Postdoc              = "Current",
+                      Faculty              = "Current",
+                      `Research Assistant` = "Current",
+                      `PhD Student`        = "Current",
+                      `Master Student`     = "Current",
+                      Alumni               = "Alumni",
+                      Visitor              = "Visitor"))
+  group <- ifelse(group==types, "", paste0("- ", group)) 
+  trim(paste0("- ", types, "\n", group))
 }
 
-null_transformer <- function(str = "NULL") {
-  function(text, envir) {
-    out <- glue::identity_transformer(text, envir)
-    if (is.null(out)) {
-      return(str)
-    }
-    out
-  }
-}
-
+research_interests <- function(x) {
+  interests <- strsplit(x, ",") %>% 
+    map(~paste0("- ", stringr::str_trim(.x)))
+  out <- map_chr(interests,
+      ~paste0("interests:\n", paste0(.x, collapse = "\n")))
+  out[is.na(x)] <- ""
+  out
+} 
 
 dat <- read_sheet("1fPfjYk2NLakqZFid12Qz_DNvuqLI5rmJWjPUpcp6g8Y")
 df <- dat %>% clean_names() %>% 
   filter(!is.na(first_name)) %>% 
-  mutate(user = tolower(glue("{last_name}-{gsub('_', '-', make_clean_names(first_name))}"))) %>% 
-  mutate(create_folder = !dir.exists(here::here("content", "authors", user)))
+  mutate(user = tolower(glue("{last_name}-{gsub('_', '-', make_clean_names(first_name))}")),
+         yaml = stringr::str_replace_all(glue_data(., 
+                          fence,
+                          trim(minimum_template),
+                          "social:",
+                          "{social_template(website, 'website')}",
+                          "{social_template(twitter, 'twitter')}",
+                          "{social_template(github, 'github')}",
+                          "{social_template(google_scholar, 'scholar')}",
+                          "{social_template(email, 'email')}",
+                          "{research_interests(interests)}",
+                          "user_groups:",
+                          ("{user_groups(type)}"),
+                          fence, 
+                          .sep = "\n"), "(?m)^\\s*$[\n\r]{1,}", "")) 
 
-out <- glue_data(df, 
-                 fence,
-                 trim(minimum_template),
-                 "{social_template(website, 'website')}",
-                 "{social_template(twitter, 'twitter')}",
-                 "{social_template(github, 'github')}",
-                 fence, 
-                 .sep = "\n") %>% 
-  stringr::str_replace_all("\n\n", "\n")
+# rewrite the _index.md for everyone
+pwalk(df, function(user, yaml, ...) {
+  user_folder <- here::here("content", "authors", user)
+  user_info <- paste0(user_folder, "/_index.md")
+  if(!dir.exists(user_folder)) dir.create(user_folder)
+  cat(yaml, file = user_info)
+})
 
-### need to write `out` to _index.md. 
+
 ### need to consider the avatar issue 
